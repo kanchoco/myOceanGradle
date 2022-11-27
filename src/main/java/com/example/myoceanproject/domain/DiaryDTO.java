185b896f67023f40f7487249ed9d26e0a72f26fe
leehoyoung <whei1234@naver.com>
2022-11-27 20:52:57 +0900

    이호영 2211272052 단위테스트

--- src/main/java/com/example/myoceanproject/domain/DiaryDTO.java
+++ src/main/java/com/example/myoceanproject/domain/DiaryDTO.java
@@ -6,11 +6,13 @@ import com.example.myoceanproject.entity.User;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
 @NoArgsConstructor
+@ToString
 public class DiaryDTO {
 
     private User user;
