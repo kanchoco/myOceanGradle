55b6a57d292e35ac1fd695cf50137d55d4efc7bb
SaeDaa1029 <spiltkim99@gmail.com>
2022-11-27 15:23:09 +0900

    saveTest DTO로 수정

--- src/test/java/com/example/myoceanproject/dtoTest/AlarmTest.java
+++ src/test/java/com/example/myoceanproject/dtoTest/AlarmTest.java
@@ -1,13 +1,19 @@
 package com.example.myoceanproject.dtoTest;
 
 import com.example.myoceanproject.domain.AlarmDTO;
+import com.example.myoceanproject.domain.UserDTO;
+import com.example.myoceanproject.entity.Alarm;
 import com.example.myoceanproject.entity.User;
 import com.example.myoceanproject.repository.AlarmRepository;
 import com.example.myoceanproject.repository.UserRepository;
 import com.example.myoceanproject.type.ReadStatus;
 import com.example.myoceanproject.type.UserAccountStatus;
 import com.example.myoceanproject.type.UserLoginMethod;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.extern.slf4j.Slf4j;
+
+import static com.example.myoceanproject.entity.QAlarm.alarm;
+import static com.example.myoceanproject.entity.QUser.user;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -15,6 +21,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Slf4j
@@ -28,19 +37,39 @@ public class AlarmTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+
 
     @Test
     public void saveAlarmTest(){
-        User user = new User("rladlsdud", "강철멘탈3", "이메일", UserAccountStatus.ACTIVE, UserLoginMethod.KAKAO, 20);
-        userRepository.save(user);
-        alarmDTO.setUser(user);
-        alarmDTO.setAlarmContent("첫 알람입니다.");
-        alarmDTO.setReadStatus(ReadStatus.READ);
-        alarmRepository.save(alarmDTO.toEntity());
+        Optional<UserDTO> userDTO = userRepository.findById(1L);
+        log.info(userDTO.get().toEntity().toString());
+        AlarmDTO alarmDTO = new AlarmDTO();
+        alarmDTO.toEntity().changeUser(userDTO.get().toEntity());
+        alarmDTO.setAlarmContent("두번째 알람입니다.");
+        alarmDTO.setReadStatus(ReadStatus.UNREAD);
 
-//        alarmRepository.findAll().stream().map(alarmDTO+"").forEach(log::info);
+//        alarmDTO.setUser(alarm1.changeUser());
+        alarmRepository.save(alarmDTO);
 
-        assertThat(alarmRepository.findAll().get(0));
+
     }
+
+//    @Test
+//    public void findAllTest(){
+//        List<Alarm> alarms = jpaQueryFactory.selectFrom(alarm)
+//                .join(alarm.user)
+//                .fetchJoin()
+//                .fetch();
+//        alarms.stream().map(Alarm::toString).forEach(log::info);
+//
+//
+//    }
+
+//    @Test
+//    public void updateTest(){
+//
+//    }
 
 }
