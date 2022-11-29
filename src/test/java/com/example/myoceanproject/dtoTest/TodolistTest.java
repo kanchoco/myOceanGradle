739ead9a25d51a1579337f0449cc566ff46a5d63
SaeDaa1029 <spiltkim99@gmail.com>
2022-11-29 13:37:29 +0900

    TodoList Diary saveTest완료 및 주석 작성

--- src/test/java/com/example/myoceanproject/dtoTest/TodolistTest.java
+++ src/test/java/com/example/myoceanproject/dtoTest/TodolistTest.java
@@ -1,62 +1,63 @@
-//package com.example.myoceanproject.dtoTest;
-//
-//import com.example.myoceanproject.domain.AlarmDTO;
-//import com.example.myoceanproject.domain.ToDoListDTO;
-//import com.example.myoceanproject.entity.Alarm;
-//import com.example.myoceanproject.entity.ToDoList;
-//import com.example.myoceanproject.entity.User;
-//import com.example.myoceanproject.repository.AlarmRepository;
-//import com.example.myoceanproject.repository.TodoListRepository;
-//import com.example.myoceanproject.repository.UserRepository;
-//import com.example.myoceanproject.type.ReadStatus;
-//import com.querydsl.jpa.impl.JPAQueryFactory;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.annotation.Rollback;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static com.example.myoceanproject.entity.QAlarm.alarm;
-//import static com.example.myoceanproject.entity.QToDoList.toDoList;
-//
-//@SpringBootTest
-//@Slf4j
-//@Transactional
-//@Rollback(false)
-//public class TodolistTest {
-//    @Autowired
-//    private TodoListRepository todoListRepository;
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    private JPAQueryFactory jpaQueryFactory;
-//
-//    @Test
-//    public void saveTest(){
-//        User user = userRepository.findById(1L).get();
-//
-//        ToDoListDTO listDTO = new ToDoListDTO();
-//
-//        listDTO.setToDoListContent("내용2");
-//        listDTO.setToDoListSelectDate(LocalDateTime.of(2003, 12, 4, 0, 0));
-//
-//        ToDoList list1 = listDTO.toEntity();
-//
-////      alarmDTO에 처음에 조회했던 유저 정보를 저장(optional이기 때문에 get 사용)
-////      changeUser 메소드로 alarmDTO에 저장된 User값을 alarm1로 전달
-//        listDTO.setUser(user);
-//        list1.changeUser(listDTO.getUser());
-//
-////      alarm 엔티티에 해당 값들을 모두 저장
-//        todoListRepository.save(list1);
-//    }
-//
+package com.example.myoceanproject.dtoTest;
+
+import com.example.myoceanproject.domain.AlarmDTO;
+import com.example.myoceanproject.domain.ToDoListDTO;
+import com.example.myoceanproject.entity.Alarm;
+import com.example.myoceanproject.entity.ToDoList;
+import com.example.myoceanproject.entity.User;
+import com.example.myoceanproject.repository.AlarmRepository;
+import com.example.myoceanproject.repository.TodoListRepository;
+import com.example.myoceanproject.repository.UserRepository;
+import com.example.myoceanproject.type.ReadStatus;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.myoceanproject.entity.QAlarm.alarm;
+import static com.example.myoceanproject.entity.QToDoList.toDoList;
+
+@SpringBootTest
+@Slf4j
+@Transactional
+@Rollback(false)
+public class TodolistTest {
+    @Autowired
+    private TodoListRepository todoListRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+
+    @Test
+    public void saveTest(){
+//        시나리오 : 유저가 투두리스트를 작성하면 작성한 유저의 아이디와 투두리스트 내용, 어떤 날짜에 해야하는 일인지가 db에 들어간다.
+        
+//        유저 정보 저장
+        User user = userRepository.findById(1L).get();
+
+//        ToDoListDTO 생성 후 값 세팅하기
+        ToDoListDTO listDTO = new ToDoListDTO();
+        listDTO.setToDoListContent("오늘할일 : 진짜 많음");
+        listDTO.setToDoListSelectDate(LocalDateTime.of(2003, 12, 4, 0, 0));
+
+//       ToDoList 엔티티화
+        ToDoList list1 = listDTO.toEntity();
+//       ToDoList 엔티티에 유저 set해줌
+        list1.changeUser(user);
+
+//      alarm 엔티티에 해당 값들을 모두 저장
+        todoListRepository.save(list1);
+    }
+
 //    @Test
 //    public void findAllTest(){
 //        List<ToDoList> lists = jpaQueryFactory.selectFrom(toDoList)
@@ -98,4 +99,4 @@
 //                .where(toDoList.toDoListId.eq(111L))
 //                .execute();
 //    }
-//}
+}
