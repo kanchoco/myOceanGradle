47c674edcd933ae28bb4a213eaeaae5b41a1ec84
SaeDaa1029 <spiltkim99@gmail.com>
2022-12-07 22:51:15 +0900

    채팅 연결 시도

--- src/main/java/com/example/myoceanproject/hadler/WebSocketHandler.java
+++ src/main/java/com/example/myoceanproject/hadler/WebSocketHandler.java
@@ -2,14 +2,11 @@ package com.example.myoceanproject.hadler;
 
 import com.example.myoceanproject.domain.ChattingDTO;
 import com.example.myoceanproject.domain.GroupDTO;
-import com.example.myoceanproject.entity.Group;
-import com.example.myoceanproject.repository.GroupRepository;
 import com.example.myoceanproject.repository.GroupRepositoryImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -22,15 +19,15 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
-    List<WebSocketSession> sessions = new ArrayList<>();
+//    List<WebSocketSession> sessions = new ArrayList<>();
     private final GroupRepositoryImpl groupRepositoryImpl;
     private final ObjectMapper objectMapper;
 
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) {
-        System.out.println("afterConnectionEstablished:" + session);
-        sessions.add(session);
-    }
+//    @Override
+//    public void afterConnectionEstablished(WebSocketSession session) {
+//        System.out.println("afterConnectionEstablished:" + session);
+//        sessions.add(session);
+//    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -39,12 +36,13 @@ public class WebSocketHandler extends TextWebSocketHandler {
         ChattingDTO chattingDTO = objectMapper.readValue(msg,ChattingDTO.class);
         log.info(chattingDTO.toString());
         GroupDTO groupDTO = groupRepositoryImpl.findGroupByGroupId(chattingDTO.getGroupId());
+        log.info(groupDTO.toString());
         groupDTO.handleMessage(session,chattingDTO,objectMapper);
     }
 
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        System.out.println("afterConnectionEstablished:" + session + ":" + status);
-    }
+//    @Override
+//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+//        System.out.println("afterConnectionEstablished:" + session + ":" + status);
+//    }
 
 }
