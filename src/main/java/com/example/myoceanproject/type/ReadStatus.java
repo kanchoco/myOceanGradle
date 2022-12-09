a3924c5ad7eea86e90dbd91c3921348395e4b915
SaeDaa1029 <spiltkim99@gmail.com>
2022-12-09 18:04:45 +0900

    채팅 버그 수정중

--- src/main/java/com/example/myoceanproject/type/ReadStatus.java
+++ src/main/java/com/example/myoceanproject/type/ReadStatus.java
@@ -1,5 +1,26 @@
 package com.example.myoceanproject.type;
 
 public enum ReadStatus {
-    READ, UNREAD
+    READ("읽음"), UNREAD("안 읽음");
+
+    private String value;
+
+    private ReadStatus(String value){this.value = value;}
+
+    public String toString(){
+        return this.value;
+    }
+
+    public static ReadStatus change(String value){
+        ReadStatus result = null;
+        for(ReadStatus status : ReadStatus.values()){
+            if(status.toString().equals(value)){
+                result = status;
+                break;
+            }
+        }
+        return result;
+    }
+
+
 }
