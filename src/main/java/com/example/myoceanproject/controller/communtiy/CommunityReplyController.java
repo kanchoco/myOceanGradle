20a1d47f0e2deba64d3926a7b742a93551230782
ghdwnstjd <dltmdgheorkf1@gmail.com>
2022-12-19 18:07:55 +0900

    [20221219 홍준성 -5]
    댓글 창 이미지 수정

--- src/main/java/com/example/myoceanproject/controller/communtiy/CommunityReplyController.java
+++ src/main/java/com/example/myoceanproject/controller/communtiy/CommunityReplyController.java
@@ -1,6 +1,8 @@
 package com.example.myoceanproject.controller.communtiy;
 
 import com.example.myoceanproject.domain.CommunityReplyDTO;
+import com.example.myoceanproject.repository.UserRepository;
+import com.example.myoceanproject.repository.UserRepositoryImpl;
 import com.example.myoceanproject.service.community.CommunityPostService;
 import com.example.myoceanproject.service.community.CommunityReplyService;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
@@ -21,12 +25,18 @@ public class CommunityReplyController {
 
     private final CommunityPostService communityPostService;
     private final CommunityReplyService communityReplyService;
+    private final UserRepositoryImpl userRepositoryImpl;
 
 //  댓글 목록 출력
     @GetMapping("/index")
-    public String getReply(Long communityPostId, Model model){
+    public String getReply(Long communityPostId, Model model, Model model2, HttpServletRequest request){
         // communityPostId를 통해 해당 ID에 속한 정보를 communityPostDTO를 받아 model 객체에 담아줌
         model.addAttribute("communityPostDTO", communityPostService.find(communityPostId));
+
+        HttpSession session = request.getSession();
+        Long userId = (Long) session.getAttribute("userId");
+        model2.addAttribute("userDTO", userRepositoryImpl.findUserById(userId));
+
         // 각 게시글의 댓글 페이지로 communityPostDTO 정보 전달
         return "app/community/community_comment";
     }
